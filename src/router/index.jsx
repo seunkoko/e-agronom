7810ee2a1af8821032ab22619166f57f28c383f0
seunkoko <seunowonikoko@gmail.com>
2019-08-25 13:08:40 +0100

    Setup react-redux

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+
+import store, { history } from '../utils/configureStore';
 
 import Home from '../pages/Home/Home';
 
 const AppContainer = (
-	<BrowserRouter>
-		<Switch>
-			<Route component={Home} />
-		</Switch>
-	</BrowserRouter>
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<Switch>
+				<Route component={Home} />
+			</Switch>
+		</ConnectedRouter>
+	</Provider>
 );
 
 export default AppContainer;
